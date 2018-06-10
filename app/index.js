67bb4371e1d140c6704838d465e1897ddff761b8
Parker Smits <psmits@gohealth.com>
2018-06-10 18:06:44 -0500

    Adding HeartRate permission

--- app/index.js
+++ app/index.js
@@ -1,38 +1,45 @@
-import { Accelerometer } from "accelerometer";
-import { Barometer } from "barometer";
 import document from "document";
 import { HeartRateSensor } from "heart-rate";
 
-let accelData = document.getElementById("accel-data");
-let barData = document.getElementById("bar-data");
 let hrmData = document.getElementById("hrm-data");
+let hrvData = document.getElementById("hrv-data");
 
-let accel = new Accelerometer();
-let bar = new Barometer();
 let hrm = new HeartRateSensor();
 
-accel.start();
-bar.start();
 hrm.start();
+
+var heartrate_array = [];
 
 function refreshData() {
   let data = {
-    accel: {
-      x: accel.x ? accel.x.toFixed(1) : 0,
-      y: accel.y ? accel.y.toFixed(1) : 0,
-      z: accel.z ? accel.z.toFixed(1) : 0
-    },
-    bar: {
-      pressure: bar.pressure ? parseInt(bar.pressure) : 0
-    },
     hrm: {
       heartRate: hrm.heartRate ? hrm.heartRate : 0
     }
   };
 
-  accelData.text = JSON.stringify(data.accel);
-  barData.text = JSON.stringify(data.bar);
-  hrmData.text = JSON.stringify(data.hrm);
+  hrmData.text = JSON.stringify(data.hrm.heartRate);
+  heartrate_array.push(data.hrm.heartRate);
+  
+  if (heartrate_array.length > 50) {
+    heartrate_array = heartrate_array.slice(heartrate_array.length - 50, heartrate_array.length);
+  } 
+  
+  if (heartrate_array.length > 5) {
+    let hrvCalc = 0;
+   
+    let squares = [];
+    for (var i = 0; i < heartrate_array.length - 1; i++) {
+      squares.push(Math.pow(heartrate_array[i] - heartrate_array[i+1], 2));
+    }
+    var sum = squares.reduce(function(a, b) { return a + b; });
+    var avg = sum / squares.length;
+
+    hrvCalc = Math.round(Math.sqrt(avg),2);
+    
+    hrvData.text = JSON.stringify(hrvCalc);
+  } else {
+    hrvData.text = "Calculating...";
+  }
 }
 
 refreshData();
